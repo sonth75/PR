0cbeb0ba5ee7f5084bf1949c02215e84614b0b68
sonth75 <sonth75.kln@gmail.com>
2026-08-18 10:54:48 +0700

    Change server listen address to '0.0.0.0'

--- server.js
+++ server.js
@@ -106,6 +106,6 @@ function getLeaderboard() {
 }
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
